d453ef97190f649d0d4bed53776ff31e28a384c1
Julien <nospam.github@gribouille.eu>
2018-10-05 10:56:46 +0200

    Added support for Google API Key on Geolocate
    Also, the raw response from Google is now available. Plus other options, such as language, to offer more accurate street names parsing.

--- src/Google/Position.php
+++ src/Google/Position.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * PHP Version 5
+ * PHP Version 7.2
  * @package Polyfony
  * @link https://github.com/SIB-FRANCE/Polyfony
  * @license http://www.gnu.org/licenses/lgpl.txt GNU General Public License
@@ -17,11 +17,21 @@ class Position {
 	private static $_api_url = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 	// return a GPS position given an address
-	public static function address($address) {
+	public static function address(
+		string $address, 
+		?array $options = []
+	) :?array {
 		// new http request
 		$request = new \Polyfony\HttpRequest();
 		// configure the request
-		$success = $request->url(self::$_api_url)->data('address', $address)->get();
+		$request->url(self::$_api_url)->data('address', $address);
+		// for each options
+		foreach($options as $key => $value) {
+			// add it to the request
+			$request->data($key, $value);
+		}
+		// execture the actual request
+		$success = $request->get();
 		// if the request succeeded
 		if($success) {
 			// get the response
@@ -31,30 +41,41 @@ class Position {
 				// if gps coordinates are available
 				if(isset($response['results'][0]['geometry']['location'])) {
 					// return formatted address and position
-					return(array(
+					return [
 						'address'	=>$response['results'][0]['formatted_address'],
-						'position'	=>$response['results'][0]['geometry']['location']
-					));
+						'position'	=>$response['results'][0]['geometry']['location'],
+						'response'	=>$response
+					];
 				}
 				// missing position
-				else { return(false); }
+				else { return null; }
 			}
 			// api did not found succeed
-			else { return(false); }
+			else { return null; }
 		}
 		// the request failed
-		else { return(false); }
+		else { return null; }
 	}
 
 	// return an address given a GPS position
-	public static function reverse($latitude, $longitude) {
+	public static function reverse(
+		float $latitude, 
+		float $longitude, 
+		?array $options = []
+	) :?array {
 		// new http request
 		$request = new \Polyfony\HttpRequest();
 		// configure the request
-		$success = $request
+		$request
 			->url(self::$_api_url)
-			->data('latlng', $latitude . ',' . $longitude)
-			->get();
+			->data('latlng', $latitude . ',' . $longitude);
+		// for each options
+		foreach($options as $key => $value) {
+			// add it to the request
+			$request->data($key, $value);
+		}
+		// actually execute the request
+		$success = $request->get();
 		// if the request succeeded
 		if($success) {
 			// get the response
@@ -64,19 +85,20 @@ class Position {
 				// if gps coordinates are available
 				if(isset($response['results'][0]['geometry']['location'])) {
 					// return formatted address and position
-					return(array(
+					return [
 						'address'	=>$response['results'][0]['formatted_address'],
-						'position'	=>$response['results'][0]['geometry']['location']
-					));
+						'position'	=>$response['results'][0]['geometry']['location'],
+						'response'	=>$response
+					];
 				}
 				// missing position
-				else { return(false); }
+				else { return null; }
 			}
 			// api did not found succeed
-			else { return(false); }
+			else { return null; }
 		}
 		// the request failed
-		else { return(false); }
+		else { return null; }
 	}
 
 
